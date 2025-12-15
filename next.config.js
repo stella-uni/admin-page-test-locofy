1f199f45c26f96c1972314c9ca5e30fd5b6d4f44
Yunhee Kim <yhkim25@inventis.co.kr>
2025-12-15 14:59:49 +0900

    오류 해결

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 const nextConfig = {
     reactStrictMode: true,
+    outputFileTracingRoot: path.join(__dirname),
     images: {
       remotePatterns: [
         {
